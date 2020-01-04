050a5dfa804685aa7018638d2ed57346edd0e819
Sean Holt <mordist1@gmail.com>
2020-01-03 19:19:13 -0500

    Removed 'toastr' call, cleaned up code a bit

--- js/main.js
+++ js/main.js
@@ -3,11 +3,7 @@
 //Big thanks to LKD07 & Gigoy for their valuable help!
 
 //Notification script
-
-
-
 //Init game data
-
 let currentQuest = null;
 
 // Quests
@@ -21,7 +17,6 @@ const Quests = [
   }
 ];
 //Currency
-  
 const Resources = {
   wood: {
     total: 0,
@@ -34,6 +29,7 @@ const Resources = {
     job: "fiberCollector"
   }
 };
+
 const Jobs = {
   lumberJack: {
     id: "lumberjackCount",
@@ -50,131 +46,133 @@ const Jobs = {
     increment: 0
   }
 }
-  
+
 let money = {
-      total: 0
+  total: 0
 },
 //Items
-    woodenKey = {
-      id: 'woodenKeyCount',
-      total: 0,
-       price: 3
-   },
-   woodenStaff = {
-       id: 'woodenStaffCount',
-       total: 0,
-       price: 10,
-       learned: false,
-       learnPrice: 40,
-       tab: 'two-tab'
-   },
+woodenKey = {
+  id: 'woodenKeyCount',
+  total: 0,
+  price: 3
+},
+woodenStaff = {
+  id: 'woodenStaffCount',
+  total: 0,
+  price: 10,
+  learned: false,
+  learnPrice: 40,
+  tab: 'two-tab'
+},
 
 //Upgrades available
-   upgrades = {
-       total: 1,
-   },
+upgrades = {
+  total: 1,
+},
 
 //Hires available
-   hires = {
-       total: 2,
-   };
+hires = {
+  total: 2,
+};
 
- 
-  //Unlockables
-  document.getElementById('two-tab').style.display = 'none';  
-  
-
-
- 
- 
-   //Round numbers to N decimals
- function roundN(num,n){
-    return parseFloat(Math.round(num * Math.pow(10, n)) /Math.pow(10,n)).toFixed(n);
+function setStyle(id, key, value) {
+  if (document.getElementById(id)) {
+    document.getElementById(id).style[key] = value;
   }
+}
+function setHtml(id, value) {
+  if (document.getElementById(id)) {
+    document.getElementById(id).innerHTML = value;
+  }
+}
+//Unlockables
+setStyle('two-tab', 'display', 'none');
 
- 
-  function updateResourceTotals() {
-	//Update page with resource numbers
-	document.getElementById('wood').innerHTML = roundN(Resources.wood.total, 2);
-  document.getElementById('fibers').innerHTML = roundN(Resources.fiber.total, 2);
-  document.getElementById('money').innerHTML = roundN(money.total, 2);
-  document.getElementById('upgradesNb').innerHTML = upgrades.total;
-  document.getElementById('hiresNb').innerHTML = hires.total;
-  document.getElementById('woodSpeed').innerHTML = Jobs.lumberJack.increment; 
-  document.getElementById('fibersSpeed').innerHTML = Jobs.fiberCollector.increment; 
-   }
+//Round numbers to N decimals
+function roundN(num,n){
+  return parseFloat(Math.round(num * Math.pow(10, n)) /Math.pow(10,n)).toFixed(n);
+}
+
+
+function updateResourceTotals() {
+  //Update page with resource numbers
+  setHtml('wood', roundN(Resources.wood.total, 2));
+  setHtml('fibers', roundN(Resources.fiber.total, 2));
+  setHtml('money', roundN(money.total, 2));
+  setHtml('upgradesNb', upgrades.total);
+  setHtml('hiresNb', hires.total);
+  setHtml('woodSpeed', Jobs.lumberJack.increment);
+  setHtml('fibersSpeed', Jobs.fiberCollector.increment);
+}
 
 function updateItemTotals() {
-	//Update page with items numbers
-	document.getElementById('woodenKey').innerHTML = woodenKey.total;
-  document.getElementById('woodenStaff').innerHTML = woodenStaff.total;
-  document.getElementById('money').innerHTML = roundN(money.total, 2);
-   }
-
+  //Update page with items numbers
+  setHtml('woodenKey', woodenKey.total);
+  setHtml('woodenStaff', woodenStaff.total);
+  setHtml('money', roundN(money.total, 2));
+}
 
 // Ressources clicker
-  function add(material) {
-        material.total = material.total + material.increment;
-        updateQuestItems(material);
-        updateResourceTotals();
-  }
+function add(material) {
+  material.total = material.total + material.increment;
+  updateQuestItems(material);
+  updateResourceTotals();
+}
 
 // Item crafter for single materiel items only
-  function craftOne(material, itemName, count) {
-            if (material.total >= count) {
-            material.total -= count;
-            itemName.total += 1;
-            updateItemTotals();
-            updateResourceTotals();
-        }   
-  }
+function craftOne(material, itemName, count) {
+  if (material.total >= count) {
+    material.total -= count;
+    itemName.total += 1;
+    updateItemTotals();
+    updateResourceTotals();
+  }   
+}
 
-  // Item crafter for double materiel items only
-  function craftTwo(material1, count1, material2, count2, itemName) {
-            if (material1.total >= count1 && material2.total >= count2) {
-            material1.total -= count1;
-            material2.total -= count2;
-            itemName.total += 1;
-            updateItemTotals();
-            updateResourceTotals();
-        }   
+// Item crafter for double materiel items only
+function craftTwo(material1, count1, material2, count2, itemName) {
+  if (material1.total >= count1 && material2.total >= count2) {
+    material1.total -= count1;
+    material2.total -= count2;
+    itemName.total += 1;
+    updateItemTotals();
+    updateResourceTotals();
+  }   
+}
+
+//Item seller
+function sellAll(item) {
+  if (item.total >= 1) {
+    money.total += (item.total * item.price);
+    item.total = 0;
+    updateItemTotals();
   }
-  
- //Item seller
-  function sellAll(item) {
-    if (item.total >= 1) {
-      money.total += (item.total * item.price);
-      item.total = 0;
-      updateItemTotals();
-    }
+}
+
+
+//Upgrades : Learn a craft
+function learn(item) {
+  if(money.total >= item.learnPrice) {
+    item.learned = true;
+    setStyle(item.tab, 'display', 'inline');
+    upgrades.total -= 1;
+    money.total -= item.learnPrice;
+    setStyle(item.id, 'display', 'none');
+    updateResourceTotals();
   }
-        
-  
-  //Upgrades : Learn a craft
-  function learn(item) {
-      
-        if(money.total >= item.learnPrice) {
-        item.learned = true;
-        document.getElementById(item.tab).style.display = 'inline';
-        upgrades.total -= 1;
-        money.total -= item.learnPrice;
-        document.getElementById(item.id).style.display = 'none';
-        updateResourceTotals();
-       }
-      }
-  
-      //Hire
-  function hire(job) {
-       if(money.total >= job.hirePrice) {
-        job.hired = true; 
-        job.increment += 0.25;
-        hires.total -= 1;
-        money.total -= job.hirePrice;
-        document.getElementById(job.id).style.display = 'none';
-        updateResourceTotals();
-    
-       }
-    }
+}
+
+//Hire
+function hire(job) {
+  if(money.total >= job.hirePrice) {
+    job.hired = true; 
+    job.increment += 0.25;
+    hires.total -= 1;
+    money.total -= job.hirePrice;
+    setStyle(job.id, 'display', 'none');
+    updateResourceTotals();
+  }
+}
 
 function updateQuestInfo() {
   let quest = currentQuest || {item: "", obtained: 0, qty: 0};
@@ -201,17 +199,16 @@ function updateQuestItems(res) {
   }
 }
 
-    //Auto-collect
-    function autoIncrement() {
-      for (var res in Resources) {
-        Resources[res].total += Jobs[Resources[res].job].increment;
-        updateQuestItems(res);
-      }
-      updateResourceTotals();
-    }
-    
-    setInterval(function(){ 
-      
-      autoIncrement();
-      checkQuest();
-    }, 1000);
+//Auto-collect
+function autoIncrement() {
+  for (var res in Resources) {
+    Resources[res].total += Jobs[Resources[res].job].increment;
+    updateQuestItems(res);
+  }
+  updateResourceTotals();
+}
+
+setInterval(function(){ 
+  autoIncrement();
+  checkQuest();
+}, 1000);
